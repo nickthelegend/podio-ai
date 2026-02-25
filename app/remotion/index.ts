e841e72dc1f4b84aeaec9151f43a1b69c7d0b9bd
nickthelegend <testingtesla7@gmail.com>
2026-02-26 00:51:59 +0530

    working?

--- app/remotion/index.ts
+++ app/remotion/index.ts
@@ -1,4 +1,4 @@
-import { registerRoot } from 'remotion';
+import { registerRoot, Composition } from 'remotion';
 import { SlideComposition } from '../../components/remotion/SlideComposition';
 import React from 'react';
 
@@ -6,7 +6,28 @@ export const RemotionRoot: React.FC = () => {
   return React.createElement(
     React.Fragment,
     null,
-    React.createElement(SlideComposition, { slides: [] })
+    React.createElement(Composition, {
+      id: "SlideVideo",
+      component: SlideComposition as React.FC<any>,
+      defaultProps: {
+        slides: [],
+        brand: undefined
+      },
+      calculateMetadata: ({ props }) => {
+        const slides = (props.slides as any[]) || [];
+        let total = 0;
+        slides.forEach((s: any) => {
+          total += s.duration ? Math.ceil(s.duration * 30) : 150;
+        });
+        if (total === 0) total = 150;
+        return {
+          durationInFrames: total,
+          fps: 30,
+          width: 1280,
+          height: 720
+        };
+      }
+    })
   );
 };
 
