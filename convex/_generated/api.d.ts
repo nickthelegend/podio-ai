3aa189ffc35f7bab41e2df691d26ce76e29976b5
nickthelegend <testingtesla7@gmail.com>
2026-03-30 00:46:43 +0530

    final repository cleanup and synchronization

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,8 +8,46 @@
  * @module
  */
 
-import type { AnyApi, AnyComponents } from "convex/server";
+import type * as http from "../http.js";
+import type * as news from "../news.js";
+import type * as projects from "../projects.js";
 
-export declare const api: AnyApi;
-export declare const internal: AnyApi;
-export declare const components: AnyComponents;
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  http: typeof http;
+  news: typeof news;
+  projects: typeof projects;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
